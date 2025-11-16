d05062ea6ea48018a0b671e9e0f5c0f6a99b4bfa
caglayan <caglayanustaa@gmail.com>
2025-11-16 22:33:18 +0300

    design sign up card

---
+++ index.js
@@ -0,0 +1,7 @@
+import * as basicLightbox from "basiclightbox";
+
+// Thanks for subscribing! A confirmation email has been sent to
+//     ash@loremcompany.com. Please open it and click the button inside to confirm
+//     your subscription. Dismiss message
+
+const instance = ""
